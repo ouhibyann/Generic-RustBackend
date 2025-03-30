70e21f07068c3ad727585f3e52d6035273ea7b20
youhib <yann.ouh300@gmail.com>
2025-03-30 15:14:09 +0200

    Loose coupling and refactor de-duplicated code

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,8 +1,10 @@
 // src/rest/mod.rs
-use actix_web::{get, web, HttpResponse, Responder};
-use serde_json::json;
+use actix_web::{get, web, Responder};
 use std::sync::Arc;
 use crate::exchanges::orderbook_provider::OrderBookProvider;
+use crate::rest::orderbook_response::order_book_response;
+
+mod orderbook_response;
 
 // Newtypes to distinguish providers.
 #[derive(Clone)]
@@ -13,26 +15,12 @@ pub struct HuobiData(pub Arc<dyn OrderBookProvider + Send + Sync>);
 
 #[get("/kraken/mid_price")]
 async fn kraken_order_book(data: web::Data<KrakenData>) -> impl Responder {
-    match data.0.get_order_book_data().await {
-        Ok(book) => HttpResponse::Ok().json(json!({
-            "best_bid": book.best_bid,
-            "best_ask": book.best_ask,
-            "mid_price": book.mid_price,
-        })),
-        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {:?}", e)),
-    }
+    order_book_response(data.0.clone()).await
 }
 
 #[get("/huobi/mid_price")]
 async fn huobi_order_book(data: web::Data<HuobiData>) -> impl Responder {
-    match data.0.get_order_book_data().await {
-        Ok(book) => HttpResponse::Ok().json(json!({
-            "best_bid": book.best_bid,
-            "best_ask": book.best_ask,
-            "mid_price": book.mid_price,
-        })),
-        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {:?}", e)),
-    }
+    order_book_response(data.0.clone()).await
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
